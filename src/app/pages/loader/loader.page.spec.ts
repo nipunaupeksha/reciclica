da5519f11f5cb797c140c5bde817ebbffecf6500
nipunaupeksha <nipuna.upeksha@gmail.com>
2022-05-07 12:25:19 +0530

    first commit

---
+++ src/app/pages/loader/loader.page.spec.ts
@@ -0,0 +1,32 @@
+import { ComponentFixture, TestBed, tick, waitForAsync } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+
+import { LoaderPage } from './loader.page';
+
+describe('LoaderPage', () => {
+  let component: LoaderPage;
+  let fixture: ComponentFixture<LoaderPage>;
+  let router: Router;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [LoaderPage],
+      imports: [IonicModule.forRoot(), AppRoutingModule],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(LoaderPage);
+    router = TestBed.get(Router);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+
+  it('should go to the login page after reload', () => {
+    spyOn(router, 'navigate');
+    component.ngOnInit();
+    tick(1500);
+    expect(router.navigate).toHaveBeenCalledWith(['login']);
+  });
+});
---
+++ src/app/pages/pickup-calls/pickup-calls.page.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+@Component({
+  selector: 'app-pickup-calls',
+  templateUrl: './pickup-calls.page.html',
+  styleUrls: ['./pickup-calls.page.scss'],
+})
+export class PickupCallsPage implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  newPickupCall(){
+    this.router.navigate(['home']);
+  }
+}
